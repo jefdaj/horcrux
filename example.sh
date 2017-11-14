a931c9634bac7eecbb38153a9650adc96b300940
Jeffrey David Johnson <jefdaj@protonmail.ch>
2017-11-14 00:27:15 +0000

    sign shares in addition to secrets

--- example.sh
+++ example.sh
@@ -21,16 +21,16 @@ run 'horcrux -v encrypt example/{encrypt.key,sign.key,secret.txt,secret.txt.gpg}
 say "and distribute the verify and decrypt keys to trustees,"
 say "along with a password share each,"
 say "then they can each verify the signature"
-run 'horcrux -v verify example/{verify.key,secret.txt.gpg*}'
+run 'horcrux -v verify example/{verify.key,secret.txt.gpg}'
 
 say "but with only 1 or 2 shares, they can't decrypt it"
 set +e
-run 'horcrux -v decrypt example/{decrypt.key,verify.key,secret.txt.gpg*,secret-decrypt-with-keys.txt,share-{01,02}.key}'
+run 'horcrux -v decrypt example/{decrypt.key,verify.key,secret.txt.gpg,secret-decrypt-with-keys.txt,share-{01,02}.key}'
 set -e
 
 say "3 shares are needed to get the password,"
 say "which is needed to get the decrypt key, which is needed to get the secret"
-run 'horcrux -v decrypt example/{decrypt.key,verify.key,secret.txt.gpg*,secret-decrypt-with-keys.txt,share-{01,04,05}.key}'
+run 'horcrux -v decrypt example/{decrypt.key,verify.key,secret.txt.gpg,secret-decrypt-with-keys.txt,share-{01,04,05}.key}'
 
 say "optionally, you can hide keys in images or sound files"
 run "horcrux -v hide example/decrypt.key example/share-01.key example.jpeg example/share-01.jpeg"
@@ -39,7 +39,7 @@ say "the hidden files can be unhidden individually"
 run "horcrux -v unhide example/{decrypt.key,share-01.jpeg,share-01-unhidden.key}"
 
 say "or it will be done automatically during decryption"
-run 'horcrux -v decrypt example/{decrypt.key,verify.key,secret.txt.gpg*,secret-decrypt-with-jpeg.txt,share-01.jpeg,share-{02,04}.key}'
+run 'horcrux -v decrypt example/{decrypt.key,verify.key,secret.txt.gpg,secret-decrypt-with-jpeg.txt,share-01.jpeg,share-{02,04}.key}'
 
 # wart: delete duplicate shares because they mess up password reconstruction
 #       (only a problem when mixing advanced cli args with autodecrypt)
