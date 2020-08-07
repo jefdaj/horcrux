6ae99dbdbcc5cad0e26b276fa03afdfd3df8ba5e
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-08-07 23:47:23 +0000

    remove test dirs all at once

--- test.sh
+++ test.sh
@@ -5,9 +5,13 @@ export PATH="$HORCRUX_DIR":$PATH
 
 cd "$HORCRUX_DIR"/tests
 
+# run test scripts in order
 for script in *.sh; do
   log_file="${script/.sh/.txt}"
-  test_dir="${script/.sh/}"
   echo -n "running ${script}... "
   . "$script" 2>&1 > "$log_file" && echo "ok" || echo "ERROR"
 done
+
+# remove test dirs at the end rather than one at a time,
+# so they can use each others' output
+find * -type d | xargs rm -r
