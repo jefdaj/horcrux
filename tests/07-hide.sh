1c41633f20d604cdd01f05472ad0d58e301c5a78
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-08-08 00:34:08 +0000

    write tests/07-hide.sh, 08-unhide.sh

---
+++ tests/07-hide.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd my-first-horcrux
+
+expect << END
+set timeout 10
+spawn horcrux hide decrypt.key horcrux-01.key ../../assets/example.jpeg horcrux-01.jpeg
+expect eof
+END
+
+file *.jpeg
