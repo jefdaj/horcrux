a3732eb5594b8c8ef778f67452a475175ea35259
jefdaj <jefdaj@users.noreply.github.com>
2020-08-08 00:01:22 +0000

    write tests/04-encrypt.sh

---
+++ tests/04-encrypt.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd my-first-horcrux
+expect << END
+set timeout 10
+spawn horcrux encrypt encrypt.key sign.key secret1.txt secret1.txt.gpg
+expect "enter a password to unlock sign.key: "
+send -- "testpass1\n"
+expect eof
+END
+sync
+file secret1.txt*
