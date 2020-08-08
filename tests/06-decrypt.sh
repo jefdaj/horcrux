73fd48d32e3ac37d232feddc0e6660572f6891c6
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-08-08 00:51:33 +0000

    find_files bugfix, 09-autoverify.sh

--- tests/06-decrypt.sh
+++ tests/06-decrypt.sh
@@ -5,9 +5,9 @@ rm secret1.txt
 
 # should work with 3 or more keys
 for n_keys in {5..1}; do
+  rm -f secret-decrypted.txt
   keys="$(ls horcrux-*.key | shuf | head -n ${n_keys})"
   echo "trying to decrypt with ${n_keys} keys..."
   horcrux decrypt decrypt.key verify.key secret1.txt.gpg secret-decrypted.txt $keys
-  rm -f secret-decrypted.txt
   echo
 done
