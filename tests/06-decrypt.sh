01b8e9fd1bb5b34719d4fc0251a6eee020f41e02
jefdaj <jefdaj@users.noreply.github.com>
2020-08-08 00:24:00 +0000

    write tests/06-decrypt.sh

---
+++ tests/06-decrypt.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd my-first-horcrux
+rm secret1.txt
+
+# should work with 3 or more keys
+for n_keys in {5..1}; do
+  keys="$(ls horcrux-*.key | shuf | head -n ${n_keys})"
+  echo "trying to decrypt with ${n_keys} keys..."
+  horcrux decrypt decrypt.key verify.key secret1.txt.gpg secret-decrypted.txt $keys
+  rm -f secret-decrypted.txt
+  echo
+done
