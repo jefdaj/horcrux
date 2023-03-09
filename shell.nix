9dddc355647d2c866b96f3e6bba6a7ec728da1d9
Jeffrey David Johnson <jefdaj@protonmail.ch>
2023-03-09 12:49:37 -0800

    shell.nix working, flake.nix not working

---
+++ shell.nix
@@ -0,0 +1,30 @@
+# This is for initial testing. Ideally it will be a flake.
+
+with import <nixpkgs> {};
+
+let
+
+  myPython = python3.withPackages (ps: with ps; [
+    docopt
+    python-gnupg
+  ]);
+
+  runDeps = with pkgs; [
+    expect
+    gnupg
+    myPython
+    openssl # TODO remove?
+    pwgen
+    qrencode
+    ssss
+    steghide
+    unzip
+  ];
+
+  devDeps = runDeps ++ (with pkgs; [
+    expect
+  ]);
+
+in pkgs.mkShell {
+  buildInputs = devDeps;
+}
