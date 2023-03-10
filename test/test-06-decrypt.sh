acf2d13c36a9a5eea9502dbd3a4a0b254c997dc2
Jeffrey David Johnson <jefdaj@protonmail.ch>
2023-03-09 21:44:32 -0800

    add more checks to test-06-decrypt.sh

--- test/test-06-decrypt.sh
+++ test/test-06-decrypt.sh
@@ -9,5 +9,9 @@ for n_keys in {1..5}; do
   keys="$(ls horcrux-*.key | shuf | head -n ${n_keys})"
   echo "trying to decrypt with ${n_keys} keys..."
   horcrux decrypt decrypt.key verify.key secret1.txt.gpg secret-decrypted.txt $keys
+  exit_code=$?
+  [[ $n_keys -lt 3 && $exit_code -ne 0 ]] && echo "(this is expected)"
+  [[ $n_keys -lt 3 && $exit_code -eq 0 ]] && echo "ERROR: succeeded with $n_keys keys"
+  [[ $n_keys -ge 3 && $exit_code -ne 0 ]] && echo "ERROR: failed with $n_keys keys"
   echo
 done
