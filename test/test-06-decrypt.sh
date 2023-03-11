a2b669d49f9c7b2e6b6eb8f96b204b039114cbf6
Jeffrey David Johnson <jefdaj@protonmail.ch>
2023-03-10 20:02:07 -0800

    clean up tests

--- test/test-06-decrypt.sh
+++ test/test-06-decrypt.sh
@@ -3,15 +3,20 @@
 cd my-first-horcrux
 rm secret1.txt
 
-# should work with 3 or more keys
+threshold=3
+
+echo "decrypt should succeed only with ${threshold}+ keys:"
+echo
+
 for n_keys in {1..5}; do
   rm -f secret-decrypted.txt
   keys="$(ls horcrux-*.key | shuf | head -n ${n_keys})"
   echo "trying to decrypt with ${n_keys} keys..."
   horcrux decrypt decrypt.key verify.key secret1.txt.gpg secret-decrypted.txt $keys
   exit_code=$?
-  [[ $n_keys -lt 3 && $exit_code -ne 0 ]] && echo "(this is expected)"
-  [[ $n_keys -lt 3 && $exit_code -eq 0 ]] && echo "ERROR: succeeded with $n_keys keys"
-  [[ $n_keys -ge 3 && $exit_code -ne 0 ]] && echo "ERROR: failed with $n_keys keys"
+  [[ $n_keys -lt $threshold && $exit_code -eq 0 ]] && echo "ERROR: succeeded with $n_keys keys"
+  [[ $n_keys -ge $threshold && $exit_code -ne 0 ]] && echo "ERROR: failed with $n_keys keys"
+  [[ $n_keys -lt $threshold && $exit_code -ne 0 ]] && echo "test passed"
+  [[ $n_keys -ge $threshold && $exit_code -eq 0 ]] && echo "test passed"
   echo
 done
